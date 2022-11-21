aef073fc958047ef70ba3969ce0ebb45c5c44b59
Suirabu <suirabu.dev@gmail.com>
2022-11-21 01:48:11 -0800

    Add main function for AVM executable

--- build.zig
+++ build.zig
@@ -10,11 +10,25 @@ pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+    
+    const exe = b.addExecutable("avm", "src/main.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
 
-    var lib_tests = b.addTest("src/test.zig");
-    lib_tests.setTarget(target);
-    lib_tests.setBuildMode(mode);
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    var exe_tests = b.addTest("src/test.zig");
+    exe_tests.setTarget(target);
+    exe_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&lib_tests.step);
+    test_step.dependOn(&exe_tests.step);
 }
---
+++ src/main.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const assam = @import("assam.zig");
+
+pub fn main() !void {
+    // Initialize allocators
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const leaked = gpa.detectLeaks();
+        if (leaked) {
+            _ = gpa.deinit();
+        }
+    }
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    // Collect command line arguments
+    var args = try std.process.argsWithAllocator(arena.allocator());
+    // Skip executable path
+    std.debug.assert(args.skip());
+    // Try to get module path
+    const module_path = args.next() orelse {
+        const stderr = std.io.getStdErr();
+        try display_usage(stderr.writer());
+        std.log.err("No source path provided", .{});
+        return error.MissingSourcePath;
+    };
+
+    const module_file = std.fs.cwd().openFile(module_path, .{}) catch |err| {
+        std.log.err("Failed to open file '{s}' for reading", .{module_path});
+        return err;
+    };
+    defer module_file.close();
+
+    const module_contents = try module_file.readToEndAlloc(arena.allocator(), std.math.maxInt(u32));
+    const module = try assam.BytecodeModule.from_bytes(module_contents, arena.allocator());
+
+    var vm = assam.VirtualMachine.init(arena.allocator());
+    defer vm.deinit();
+
+    for (module.instructions) |instruction| {
+        try vm.execute_instruction(instruction);
+    }
+
+    const snapshot = vm.get_snapshot();
+    std.debug.print("VM output: {any}\n", .{snapshot.data_stack});
+}
+
+fn display_usage(writer: anytype) !void {
+    try writer.print(
+        \\avm - Assam vitual machine
+        \\
+        \\Usage:
+        \\    avm <source>
+        \\
+    , .{});
+}
