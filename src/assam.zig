443fb59bd55b5028de2ae9f2d441cd99d9d4d886
Suirabu <suirabu.dev@gmail.com>
2022-11-24 02:09:28 -0800

    Add `AssamError` error type
    Currently the only error set our APIs return from is
    `VirtualMachineError`. Eventually we'll amass more error sets, at
    which point it would make sense to merge them into a single unified
    error set.
    
    This commit does exactly this by exposing a new `AssamError` error set
    in `assam.zig`. This new error set can be used to contain any other
    error sets which may be created in the future.

--- src/assam.zig
+++ src/assam.zig
@@ -1,7 +1,9 @@
 const vm = @import("vm.zig");
 pub const VirtualMachine = vm.VirtualMachine;
-pub const VirtualMachineError = vm.VirtualMachineError;
+const VirtualMachineError = vm.VirtualMachineError;
 
 const instruction = @import("instruction.zig");
 pub const Instruction = instruction.Instruction;
 pub const InstructionTag = instruction.InstructionTag;
+
+pub const AssamError = VirtualMachineError;
